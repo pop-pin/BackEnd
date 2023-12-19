ea90fbcbd6b1ad66bc2ad0218d3f24aa4a249229
codrin2 <hssang127@gmail.com>
2023-12-19 16:50:19 +0900

    feat: submodule setting

---
+++ src/test/java/com/poppin/poppinsubmodule/PoppinSubmoduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poppin.poppinsubmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoppinSubmoduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
